5439883c2719109827c282b861cc9e25662c229a
chacha <yushin.pak@gmail.com>
2024-05-31 15:04:02 +0900

    Feat: 다이어리 에디터 레이아웃 완성

--- src/pages/diary/DiaryEditor.tsx
+++ src/pages/diary/DiaryEditor.tsx
@@ -1,1 +1,66 @@
+import React from "react";
+import styled from "styled-components";
 
+// 컴포넌트
+import Button from "../../components/basedComponent/Button";
+
+// 임시 데이터
+import { tempTitle, tempContent } from "../../../tempData";
+
+interface DiaryViewerProps {
+  // 추후 그냥 diary props와 합
+  // 추후 다 필수 값으로 변경
+  title?: string;
+  date?: string; // 추후 알아보고 타입 변경. string인지 아닌지 제대로 모르겠음
+  content?: string;
+}
+
+const EditorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  & > * {
+    box-sizing: border-box;
+    padding: 10px 0;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Title = styled.input`
+  font-size: var(--font-size-hd2);
+  font-weight: var(--font-weight-bold); // 추후 - input에서의 weight와  h2에서의 weight이 왜 다를까..?
+  background-color: transparent;
+  border: none;
+
+`;
+
+const Content = styled.textarea`
+  // 초기값에 따른 높이 조절은 코드를 통해 해야함
+  font-size: var(--font-size-md1);
+  font-weight: var(--font-weight-medium); //추후! 이거 좀 이상한 것 같음
+  min-height: 90vh;
+  height: 100vh; // 추후 코드로 auto가 되도록 조정
+  white-space: pre-wrap; // 추후 더 상위 컴포넌트에서 조정할 수 있도록 수정
+  line-height: var(--line-height-default);
+  border: none;
+  background-color: transparent;
+`;
+
+const DiaryEditor: React.FC<DiaryViewerProps> = ({ title, date, content }) => {
+  return (
+    <EditorContainer>
+      <Title value={tempTitle} />
+      <Content>{tempContent}</Content>
+      <ButtonContainer>
+        <Button>고쳐쓰기 완료</Button>
+      </ButtonContainer>
+    </EditorContainer>
+  );
+};
+
+export default DiaryEditor;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -7,6 +7,7 @@ import Layout from "../components/global/Layout";
 import Login from "../pages/login/Login";
 import Home from "../pages/home/Home";
 import DiaryViewer from "../pages/diary/DiaryViewer";
+import DiaryEditor from "../pages/diary/DiaryEditor";
 
 
 const AppRouter = () => {
@@ -16,7 +17,7 @@ const AppRouter = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Layout component={Home} />} />
         {/* <Route path="/mypage" element={<Layout component={Mypage} />} /> */}
-        {/* <Route path="/diary-editor" element={<Layout component={DiaryEditor} />} /> */}
+        <Route path="/diary-editor" element={<Layout component={DiaryEditor} />} />
         {/* 뷰어는 세부적으로 좀 더 자세하게 숫자도 쓰고 해야댕 */}
         <Route path="/diary-viewer" element={<Layout component={DiaryViewer} />} />
         {/* <Route path="/register" element={<Layout component={Register} />} /> */}
